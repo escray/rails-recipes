1e0456a14e533c940e503cd1cfb4e32afc100b2d
escray <zescray@gmail.com>
2017-10-27 17:04:19 +0800

    multi language setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-
   devise_for :users
 
+  get '/faq' => 'pages#faq'
   resources :events
 
   namespace :admin do
-    root "events#index"
+    root 'events#index'
     resources :events
   end
 
-  root "events#index"
-
+  root 'events#index'
 end
