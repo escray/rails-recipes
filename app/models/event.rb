808f9d98d1d5efa5d1de42e18f762aeb52dac215
escray <zescray@gmail.com>
2017-10-29 17:32:10 +0800

    select

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 #
 class Event < ApplicationRecord
+  STATUS = %w[draft public private].freeze
   validates_presence_of :name, :friendly_id
   validates_uniqueness_of :friendly_id
   validates_format_of :friendly_id, with: /\A[a-z0-9\-]+\z/
+  validates_inclusion_of :status, in: STATUS
 
   before_validation :generate_friendly_id, on: :create
 
