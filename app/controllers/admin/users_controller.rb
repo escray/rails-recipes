416112b5e193e6d4ea6ef9ef3c92a6083390e4a5
escray <zescray@gmail.com>
2017-10-30 15:24:17 +0800

    nested form

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,23 @@
+#
+class Admin::UsersController < AdminController
+  def index
+    @user = User.includes(:groups).all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path
+    else
+      render 'edit'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, group_ids: [])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def display_name
-    self.email.split("@").first
-  end
+  has_many :memberships
+  has_many :groups, through: :memberships
 
+  has_one :profile
+
+  accepts_nested_attributes_for :profile
+
+  def display_name
+    email.split('@').first
+  end
 end
