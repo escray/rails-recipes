863ed6a342ab2f60fd3641534dc7ec9cd8ebbc3b
escray <zescray@gmail.com>
2017-11-03 09:28:24 +0800

    split css and js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,4 @@
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
 //= require bootstrap-sprockets
-//= require select2
-//= require nested_form_fields
 //= require_tree .
