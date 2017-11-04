368c1a9a05f3998796a592a1b7102f64ac040c83
escray <zescray@gmail.com>
2017-11-05 07:16:24 +0800

    18-4 filter multiple select

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -6,13 +6,18 @@ class Admin::EventRegistrationsController < ApplicationController
     @registrations = @event.registrations.includes(:ticket)
                            .order('id DESC').page(params[:page]).per(7)
 
-    if params[:status].present? && Registration::STATUS.include?(params[:status])
-      @registrations = @registrations.by_status(params[:status])
-    end
+    @registrations = @registrations.by_status(params[:status]) if
+     Registration::STATUS.include?(params[:status]) && params[:status]
+     .present?
 
-    if params[:ticket_id].present?
-      @registrations = @registrations.by_ticket(params[:ticket_id])
-    end
+    @registrations = @registrations.by_ticket(params[:ticket_id]) if
+      params[:ticket_id].present?
+
+    @registrations = @registrations.by_status(params[:statuses]) if
+      Array(params[:statuses]).any?
+
+    @registrations = @registrations.by_ticket(params[:ticket_ids]) if
+      Array(params[:ticket_ids]).any?
   end
 
   def destroy
